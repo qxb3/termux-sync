0821dd6b3fd23de55af99736b4518cbf8a12d0b8
qxb3 <toolong972@gmail.com>
2021-10-21 06:28:19 +0800

    update

--- termux-sync.sh
+++ termux-sync.sh
@@ -7,15 +7,16 @@ fi
 
 dir1=$( realpath "$1" )
 dir2=$( realpath "$2" )
+projname=$( basename "$dir1" )
 
 # Copy the project
-mkdir "$dir2/"
-rsync -au --progress --exclude "node_modules" "$1/" "$dir2/"
+mkdir "$dir2/$projname"
+rsync -au --progress --exclude "node_modules" "$dir1/" "$dir2/$projname"
 clear
 
 # Syncing process
 echo "Running..."
 while :
 do
-  rsync -au --delete --exclude "node_modules" "$dir2/" "$dir1/"
+  rsync -au --delete --exclude "node_modules" "$dir2/$projname/" "$dir1/"
 done
