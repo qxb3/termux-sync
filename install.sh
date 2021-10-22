794f3c101fb4dfeb182ee8213f09c5d076abe15d
qxb3 <toolong972@gmail.com>
2021-10-23 01:02:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 chmod +x termux-sync.sh
 mv termux-sync.sh "$PREFIX/bin/termux-sync"
+
+if [ ! -f $PREFIX/bin/rsync ]; then
+  apt install rsync -y
+fi
+
+if [ ! -f $PREFIX/bin/inotifywait ]; then
+  apt install inotify-tools
+fi
+
+echo "Done!"
